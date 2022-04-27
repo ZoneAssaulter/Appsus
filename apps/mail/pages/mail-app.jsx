b28565a1bea2a451b45a958ba05c138a90fec3bd
Baruch <baruch.rozenberg93@gmail.com>
2022-04-27 20:02:12 +0300

    stuck at email service bug

--- apps/mail/pages/mail-app.jsx
+++ apps/mail/pages/mail-app.jsx
@@ -1,18 +1,79 @@
-import {eventBusService} from '../services/event-bus.service.js'
-import {utilService} from '../services/util.service.js'
-import {emailService} from '../apps/mail/services/email.service.js'
-
+import { eventBusService } from '../../../services/event-bus.service.js'
+import { utilService } from '../../../services/util.service.js'
+import { emailService } from '../services/email.service.js'
 
 
 export class MailApp extends React.Component {
+  state = {
+    emails: [],
+    criteria: {
+      status: 'inbox',
+      txt: '',
+      isRead: undefined,
+      isStarred: undefined,
+    },
+    isShowCompose: false,
+    sort: {
+      type: 'byDate',
+      order: 1,
+    },
+    noteEmail: {
+      subject: '',
+      body: '',
+    },
+  }
 
-    state ={}
+  componentDidMount() {
+    window.scrollTo(0, 0)
+    // this.loadEmails()
+    this.removeEventBus()
+    this.searchParams()
+  }
 
-    render(){
-        return (
-            <section className="mail-app">
-                mail app
-            </section>
-        )
+  
+
+  componentWillUnmount() {
+    this.removeEventBus()
+  }
+
+  removeEventBus = () => {
+    eventBusService.on('search', (txt) => this.debouncedFunc({ txt }))
+  }
+
+  searchParams = () => {
+    const query = new URLSearchParams(this.props.location.search)
+    const subject = query.get('subject')
+    const body = query.get('body')
+    this.setState({ noteEmail: { subject, body } })
+    if (subject || body) {
+      this.setState({ isShowCompose: true })
     }
+  }
+
+  debouncedFunc = utilService.debounce(this.onSetCriteria, 10)
+
+  loadEmails = () => {
+    console.log('loading emails')
+    const { criteria, sort } = this.state
+    console.log('criteria:', criteria, 'sort:', sort)
+    emailService.query(criteria, sort).then((emails) => {
+      console.log('query().then')
+      this.setState({ emails })
+      this.props.history.push('/mailapp')
+    })
+  }
+
+  render() {
+    emailService.testFunc()
+    
+    return (
+      <section className='mail-app'>
+        <aside className='aside-container'>
+          <button className='compose-btn' onClick={this.onToggleCompose}>
+            <img src='./assets/img/compose-plus.png' />
+          </button>
+        </aside>
+      </section>
+    )
+  }
 }
