35f04199dcedb2ba7864f2beaf173b5e798603e9
Baruch <baruch.rozenberg93@gmail.com>
2022-04-27 13:26:07 +0300

    added some stuff to header

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -1,19 +1,35 @@
 const { NavLink, Link, withRouter } = ReactRouterDOM
 
 class _AppHeader extends React.Component {
-  state = {}
+  state = {
+    isShowMenu: false,
+    filterText: '',
+    nameApp: '',
+  }
 
-  
+  getNameApp = () => {
+    const path = this.props.location.pathname
+    const removeAfterIdx = path.indexOf('app')
+    this.setState({ nameApp: path.substring(1, removeAfterIdx) })
+  }
 
   render() {
     return (
       <header className='app-header'>
-        <Link to='/' className='logo'>
-          <h1>Appsus</h1>
-        </Link>
-        <NavLink to='/keepapp'>keep app</NavLink>
-        <NavLink to='/mailapp'>mail app</NavLink>
-        <NavLink to='/bookapp'>book app</NavLink>
+        <nav className='main-nav'>
+          <Link to='/' className='logo'>
+            <h1>Appsus</h1>
+          </Link>
+          <NavLink to='/keepapp' onClick={this.onToggleAppsMenu}>
+            <div></div>
+          </NavLink>
+          <NavLink to='/mailapp' onClick={this.onToggleAppsMenu}>
+            mail app
+          </NavLink>
+          <NavLink to='/bookapp' onClick={this.onToggleAppsMenu}>
+            book app
+          </NavLink>
+        </nav>
       </header>
     )
   }
