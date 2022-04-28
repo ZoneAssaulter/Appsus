91ff4acd3451590ac99162a799a527172652244e
Baruch <baruch.rozenberg93@gmail.com>
2022-04-28 19:39:26 +0300

    email is done

--- apps/mail/cmps/email-compose.jsx
+++ apps/mail/cmps/email-compose.jsx
@@ -19,29 +19,18 @@ export class EmailCompose extends React.Component {
   }
 
   loadEmail = () => {
-    const { emailId, noteEmail } = this.props
-    if (!emailId && noteEmail) return
-    else if (emailId)
-      emailService.getEmailById(emailId).then((email) => {
-        const setId = email.status === 'draft' ? email.id : ''
-        this.setState({
-          email: {
-            toUser: email.from,
-            subject: email.subject,
-            body: email.body,
-            id: setId,
-          },
-        })
-      })
-    else
+    const { emailId , noteEmail } = this.props
+    if (!emailId && !noteEmail) return
+    else if(emailId) emailService.getEmailById(emailId).then((email) => {
+      const setId = email.status === 'draft' ? email.id : ''
       this.setState({
-        email: {
-          toUser: '',
-          subject: noteEmail.subject,
-          body: noteEmail.body,
-          id: '',
-        },
+        email: { toUser: email.from, subject: email.subject, body: email.body,
+                  id: setId},
       })
+    })
+    else this.setState({
+      email: {toUser: '', subject: noteEmail.subject, body: noteEmail.body, id: ''}
+    })
   }
 
   handleChange = ({ target }) => {
@@ -56,8 +45,7 @@ export class EmailCompose extends React.Component {
     ev.preventDefault()
     const { email } = this.state
     emailService.saveEmail(email, status).then((email) => {
-      const message =
-        email.status === 'draft' ? 'Saved email as draft' : 'Email sent'
+      const message = (email.status === 'draft') ? 'Saved email as draft' : 'Email sent'
       eventBusService.emit('user-msg', { txt: message, type: 'success' })
       this.props.loadEmails()
       this.props.onToggleCompose()
@@ -66,54 +54,47 @@ export class EmailCompose extends React.Component {
 
   render() {
     const { toUser, subject, body } = this.state.email
+
     return (
-      <section className='email-compose'>
+      <section className="email-compose">
         <form onSubmit={(ev) => this.onSaveEmail(ev, 'sent')}>
-          <div
-            className='email-compose-header'
-            onClick={(ev) => this.onSaveEmail(ev, 'draft')}
-          >
-            <i className='fas fa-times'></i>
+          <div className="email-compose-header" onClick={(ev) => this.onSaveEmail(ev,'draft')}>
+            <i className="fas fa-times"></i>
           </div>
-          <div className='form-to'>
-            <label htmlFor='to-user'>To:</label>
+          <div className="form-to">
+            <label htmlFor="to-user">To:</label>
             <input
               ref={this.inputRef}
-              type='text'
-              name='toUser'
-              id='to-user'
+              name="toUser"
+              type="text"
+              id="to-user"
               value={toUser ? toUser : ''}
               onChange={this.handleChange}
-              autoComplete='off'
+              autoComplete="off"
             />
           </div>
-          <div className='form-subject'>
-            <label htmlFor='subject'>Subject:</label>
+          <div className="form-subject">
+            <label htmlFor="subject">Subject:</label>
             <input
-              type='text'
-              name='subject'
-              id='subject'
+              name="subject"
+              type="text"
+              id="subject"
               value={subject ? subject : ''}
               onChange={this.handleChange}
-              autoComplete='off'
+              autoComplete="off"
             />
-            <textarea
-              name='body'
-              rows='18'
-              value={body ? body : ''}
-              onChange={this.handleChange}
-            />
-            <div className='form-btns'>
-              <button type='submit' className='form-send-btn'>
-                Send
-              </button>
-              <button
-                className='form-trash-btn'
-                onClick={this.props.onToggleCompose}
-              >
-                <i className='fas fa-trash'></i>
-              </button>
-            </div>
+          </div>
+          <textarea
+            name="body"
+            rows="18"
+            value={body ? body : ''}
+            onChange={this.handleChange}
+          />
+          <div className="form-btns">
+            <button type="submit" className="form-send-btn">Send</button>
+            <button className="form-trash-btn" onClick={this.props.onToggleCompose}>
+              <i className="fas fa-trash"></i>
+            </button>
           </div>
         </form>
       </section>
